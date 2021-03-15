f5186935a255a20913c046ade5aab9d652055a2b
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-03-15 11:08:46 +0700

    more motors functions

--- hardware/firmware/motor.c
+++ hardware/firmware/motor.c
@@ -5,67 +5,154 @@
 
 #include "motor.h"
 
-extern SerialUSBDriver SDU1;
+uint8_t motor1_run = MOTOR_OFF;
+static uint8_t motor1_dir = MOTOR1_CW;
+static uint16_t motor1_step;
 
-static void motor1_run(uint8_t direction, uint16_t step, uint16_t speed){
+uint8_t motor2_run = MOTOR_OFF;
+static uint8_t motor2_dir = MOTOR2_CW;
+static uint16_t motor2_step;
+
+uint8_t motor3_run = MOTOR_OFF;
+static uint8_t motor3_dir = MOTOR3_CW;
+static uint16_t motor3_step;
+
+uint8_t motor4_run = MOTOR_OFF;
+static uint8_t motor4_dir = MOTOR4_CW;
+static uint16_t motor4_step;
+
+static THD_WORKING_AREA(waMot1_Run, 128);
+static THD_FUNCTION(thdMot1_Run, arg) {
+    (void)arg;
     uint16_t stepItr;
 
-    if(direction==0){palClearPad(MOTOR1_PORT,MOTOR1_DIR);}
-    else{palSetPad(MOTOR1_PORT,MOTOR1_DIR);}
+    palSetPadMode(MOTOR1_PORT,MOTOR1_DIR,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(MOTOR1_PORT,MOTOR1_STEP,PAL_MODE_OUTPUT_PUSHPULL);
+    palClearPad(MOTOR1_PORT,MOTOR1_DIR);
+    palClearPad(MOTOR1_PORT,MOTOR1_STEP);
 
-    for(stepItr=0;stepItr<step;stepItr++){
-        palSetPad(MOTOR1_PORT,MOTOR1_STEP);
-        chThdSleepMicroseconds(speed);
-        palClearPad(MOTOR1_PORT,MOTOR1_STEP);
-        chThdSleepMicroseconds(speed);
+    chRegSetThreadName("motor1_run");
+    while (true) {
+        if(motor1_run==MOTOR_ON){
+            if(motor1_dir==MOTOR1_CW){palClearPad(MOTOR1_PORT,MOTOR1_DIR);}
+            else{palSetPad(MOTOR1_PORT,MOTOR1_DIR);}
+
+            for(stepItr=0;stepItr<motor1_step;stepItr++){
+                palSetPad(MOTOR1_PORT,MOTOR1_STEP);
+                chThdSleepMicroseconds(MOTOR_SPEED);
+                palClearPad(MOTOR1_PORT,MOTOR1_STEP);
+                chThdSleepMicroseconds(MOTOR_SPEED);
+            }
+            motor1_run = MOTOR_OFF;
+        }
+        else{motor1_run = MOTOR_OFF;}
+
+        chThdSleepMilliseconds(100);
     }
 }
 
-static void motor2_run(uint8_t direction, uint16_t step, uint16_t speed){
+static THD_WORKING_AREA(waMot2_Run, 128);
+static THD_FUNCTION(thdMot2_Run, arg) {
+    (void)arg;
     uint16_t stepItr;
 
-    if(direction==0){palClearPad(MOTOR2_PORT,MOTOR2_DIR);}
-    else{palSetPad(MOTOR2_PORT,MOTOR2_DIR);}
+    palSetPadMode(MOTOR2_PORT,MOTOR2_DIR,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(MOTOR2_PORT,MOTOR2_STEP,PAL_MODE_OUTPUT_PUSHPULL);
+    palClearPad(MOTOR2_PORT,MOTOR2_DIR);
+    palClearPad(MOTOR2_PORT,MOTOR2_STEP);
 
-    for(stepItr=0;stepItr<step;stepItr++){
-        palSetPad(MOTOR2_PORT,MOTOR2_STEP);
-        chThdSleepMicroseconds(speed);
-        palClearPad(MOTOR2_PORT,MOTOR2_STEP);
-        chThdSleepMicroseconds(speed);
+    chRegSetThreadName("motor2_run");
+    while (true) {
+        if(motor2_run==MOTOR_ON){
+            if(motor2_dir==MOTOR2_CW){palClearPad(MOTOR2_PORT,MOTOR2_DIR);}
+            else{palSetPad(MOTOR2_PORT,MOTOR2_DIR);}
+
+            for(stepItr=0;stepItr<motor2_step;stepItr++){
+                palSetPad(MOTOR2_PORT,MOTOR2_STEP);
+                chThdSleepMicroseconds(MOTOR_SPEED);
+                palClearPad(MOTOR2_PORT,MOTOR2_STEP);
+                chThdSleepMicroseconds(MOTOR_SPEED);
+            }
+            motor2_run = MOTOR_OFF;
+        }
+        else{motor2_run = MOTOR_OFF;}
+
+        chThdSleepMilliseconds(100);
     }
 }
 
-static void motor3_run(uint8_t direction, uint16_t step, uint16_t speed){
+static THD_WORKING_AREA(waMot3_Run, 128);
+static THD_FUNCTION(thdMot3_Run, arg) {
+    (void)arg;
     uint16_t stepItr;
 
-    if(direction==0){palClearPad(MOTOR3_PORT,MOTOR3_DIR);}
-    else{palSetPad(MOTOR3_PORT,MOTOR3_DIR);}
+    palSetPadMode(MOTOR3_PORT,MOTOR3_DIR,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(MOTOR3_PORT,MOTOR3_STEP,PAL_MODE_OUTPUT_PUSHPULL);
+    palClearPad(MOTOR3_PORT,MOTOR3_DIR);
+    palClearPad(MOTOR3_PORT,MOTOR3_STEP);
 
-    for(stepItr=0;stepItr<step;stepItr++){
-        palSetPad(MOTOR3_PORT,MOTOR3_STEP);
-        chThdSleepMicroseconds(speed);
-        palClearPad(MOTOR3_PORT,MOTOR3_STEP);
-        chThdSleepMicroseconds(speed);
+    chRegSetThreadName("motor3_run");
+    while (true) {
+        if(motor3_run==MOTOR_ON){
+            if(motor3_dir==MOTOR3_CW){palClearPad(MOTOR3_PORT,MOTOR3_DIR);}
+            else{palSetPad(MOTOR3_PORT,MOTOR3_DIR);}
+
+            for(stepItr=0;stepItr<motor3_step;stepItr++){
+                palSetPad(MOTOR3_PORT,MOTOR3_STEP);
+                chThdSleepMicroseconds(MOTOR_SPEED);
+                palClearPad(MOTOR3_PORT,MOTOR3_STEP);
+                chThdSleepMicroseconds(MOTOR_SPEED);
+            }
+            motor3_run = MOTOR_OFF;
+        }
+        else{motor3_run = MOTOR_OFF;}
+
+        chThdSleepMilliseconds(100);
     }
 }
 
-static void motor4_run(uint8_t direction, uint16_t step, uint16_t speed){
+static THD_WORKING_AREA(waMot4_Run, 128);
+static THD_FUNCTION(thdMot4_Run, arg) {
+    (void)arg;
     uint16_t stepItr;
 
-    if(direction==0){palClearPad(MOTOR4_PORT,MOTOR4_DIR);}
-    else{palSetPad(MOTOR4_PORT,MOTOR4_DIR);}
+    palSetPadMode(MOTOR4_PORT,MOTOR4_DIR,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(MOTOR4_PORT,MOTOR4_STEP,PAL_MODE_OUTPUT_PUSHPULL);
+    palClearPad(MOTOR4_PORT,MOTOR4_DIR);
+    palClearPad(MOTOR4_PORT,MOTOR4_STEP);
 
-    for(stepItr=0;stepItr<step;stepItr++){
-        palSetPad(MOTOR4_PORT,MOTOR4_STEP);
-        chThdSleepMicroseconds(speed);
-        palClearPad(MOTOR4_PORT,MOTOR4_STEP);
-        chThdSleepMicroseconds(speed);
+    chRegSetThreadName("motor4_run");
+    while (true) {
+        if(motor4_run==MOTOR_ON){
+            if(motor4_dir==MOTOR4_CW){palClearPad(MOTOR4_PORT,MOTOR4_DIR);}
+            else{palSetPad(MOTOR4_PORT,MOTOR4_DIR);}
+
+            for(stepItr=0;stepItr<motor4_step;stepItr++){
+                palSetPad(MOTOR4_PORT,MOTOR4_STEP);
+                chThdSleepMicroseconds(MOTOR_SPEED);
+                palClearPad(MOTOR4_PORT,MOTOR4_STEP);
+                chThdSleepMicroseconds(MOTOR_SPEED);
+            }
+            motor4_run = MOTOR_OFF;
+        }
+        else{motor4_run = MOTOR_OFF;}
+
+        chThdSleepMilliseconds(100);
+    }
+}
+
+void motor_Run(uint8 mot_num, uint8_t mot_dir, uint16_t mot_step){
+    switch(mot_num){
+        case MOTOR1: motor1_dir = mot_dir; motor1_step = mot_step; motor1_run = MOTOR_ON; break;
+        case MOTOR2: motor2_dir = mot_dir; motor2_step = mot_step; motor2_run = MOTOR_ON; break;
+        case MOTOR3: motor3_dir = mot_dir; motor3_step = mot_step; motor3_run = MOTOR_ON; break;
+        case MOTOR4: motor4_dir = mot_dir; motor4_step = mot_step; motor4_run = MOTOR_ON; break;
     }
 }
 
 void motor_Init(void){
-    palSetPadMode(MOTOR2_PORT,MOTOR2_DIR,PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPadMode(MOTOR2_PORT,MOTOR2_STEP, PAL_MODE_OUTPUT_PUSHPULL);
-    palClearPad(MOTOR2_PORT,MOTOR2_DIR);
-    palClearPad(MOTOR2_PORT,MOTOR2_STEP);
+    chThdCreateStatic(waMot1_Run, sizeof(waMot1_Run), NORMALPRIO, thdMot1_Run, NULL);
+    chThdCreateStatic(waMot2_Run, sizeof(waMot2_Run), NORMALPRIO, thdMot2_Run, NULL);
+    chThdCreateStatic(waMot3_Run, sizeof(waMot3_Run), NORMALPRIO, thdMot3_Run, NULL);
+    chThdCreateStatic(waMot4_Run, sizeof(waMot4_Run), NORMALPRIO, thdMot4_Run, NULL);
 }
--- hardware/firmware/motor.h
+++ hardware/firmware/motor.h
@@ -5,23 +5,37 @@
 #define MOTOR1_PORT GPIOB
 #define MOTOR1_STEP 1
 #define MOTOR1_DIR  0
+#define MOTOR1_CW   0
+#define MOTOR1_CCW  1
 
 #define MOTOR2      2
 #define MOTOR2_PORT GPIOB
 #define MOTOR2_STEP 11
 #define MOTOR2_DIR  10
+#define MOTOR2_CW   0
+#define MOTOR2_CCW  1
 
 #define MOTOR3      3
 #define MOTOR3_PORT GPIOB
 #define MOTOR3_STEP 13
 #define MOTOR3_DIR  12
+#define MOTOR3_CW   0
+#define MOTOR3_CCW  1
 
 #define MOTOR4      4
 #define MOTOR4_PORT GPIOB
 #define MOTOR4_STEP 15
 #define MOTOR4_DIR  14
+#define MOTOR4_CW   0
+#define MOTOR4_CCW  1
 
-void motor_Run(uint8_t motor, uint8_t direction, uint16_t step, uint16_t speed);
+#define MOTOR_SPEED 500
+#define MOTOR_CW    0
+#define MOTOR_CCW   1
+#define MOTOR_ON    1
+#define MOTOR_OFF   0
+
+void motor_Run(uint8 mot_num, uint8_t mot_dir, uint16_t mot_step);
 void motor_Init(void);
 
 #endif
